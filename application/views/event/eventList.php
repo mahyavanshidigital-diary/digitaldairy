325a845ee3f1948b65b2eb52028bcad92afab5bb
bhavincrest <bhavin.s@crestinfosystems.com>
2016-03-22 09:48:59 +0530

    Adding Event SqlScript

--- application/views/event/eventList.php
+++ application/views/event/eventList.php
@@ -7,25 +7,14 @@
                     <hr>
                 </div>
                 <?php 
-                        echo "<pre>";
-                            print_r($eventList);
-                        echo "</pre>";
+                foreach($eventList as $eventEach) {
                 ?>
                 <div class="col-md-12">
-                    <h1 class="page-header">Garba Party in Marriage</h1>
-
-                    <p>This is a great place to introduce your company or project and describe what you do.</p>
-                    <p>Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets.</p>
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                    <h1 class="page-header"><?php echo $eventEach['event_title']; ?></h1>
+                    <p><?php echo str_replace('/n','<br/>',$eventEach['event_title']); ?></p>
                 </div>
                 <div class="clearfix"></div>
-                <div class="col-md-12">
-                    <h1 class="page-header">Garba Party in Marriage</h1>
-
-                    <p>This is a great place to introduce your company or project and describe what you do.</p>
-                    <p>Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets.</p>
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                </div>
+                <?php } ?>
             </div>
     </div>
 </div>
